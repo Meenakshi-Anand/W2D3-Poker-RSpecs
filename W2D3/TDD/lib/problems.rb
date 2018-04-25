eeb5d75ff7451edfa4c584ec5f148999eedec44f
Meenakshi <abhi@codeanand.com>
2018-04-25 14:58:47 -0700

    finished array exercises

--- W2D3/TDD/lib/problems.rb
+++ W2D3/TDD/lib/problems.rb
@@ -1,3 +1,4 @@
+require 'byebug'
 class Array 
   
   def remove_dups
@@ -14,6 +15,34 @@ class Array
     results     
   end 
   
+  def my_transpose 
+    results = []
+    self.each_with_index do |row, idx1| 
+      rows = []
+      (0...row.size).each do |idx2| 
+        rows.push(self[idx2][idx1])
+      end 
+      results.push(rows)
+    end 
+    results 
+  end
+  
+  def stock_picker
+    raise "Need at least two days of data for all stocks!" if size < 2 
+    most_profit = [] # this will return an array of [day_to_buy, day_to_sell]
+    highest_value = 0
+    size.times do |start| 
+      (start + 1...size).each do |stop| 
+        value = self[stop] - self[start]
+        if value > highest_value
+          highest_value = value 
+          most_profit = [start, stop]
+        end 
+      end 
+    end  
+    most_profit
+  end 
+   
   protected
    
   def my_uniq
@@ -23,6 +52,7 @@ class Array
     end
     results  
   end
+  
   
   
 end 
--- W2D3/TDD/spec/problems_spec.rb
+++ W2D3/TDD/spec/problems_spec.rb
@@ -2,11 +2,7 @@ require 'problems'
 require 'rspec'
 
 describe "Array Exercises" do
-  subject(:array) { Array.new } 
-  
-  before(:each) do 
-    array = [1,2,1,3,3]
-  end
+    let(:array) { [1,2,1,3,3] }
   
   describe "#remove_dups" do
     it "should remove duplicate elements from an array" do 
@@ -20,12 +16,64 @@ describe "Array Exercises" do
   end
     
   describe "#two_sum" do 
+    let(:array) { [1, 3, -1, -2, 4, -3, 2] }
+    let(:array1) { [1,2,1,3,3] }
+    
     it "should return the indices of elements that add up to 0" do 
-      array = [1, 3, -1, -2, 4, -3, 2]
       expect(array.two_sum).to eq([[0,2], [1, 5], [3,6]])
     end
     it "should return an empty array if no two indices ad up to 0" do 
-      expect(array.two_sum).to eq([])
+      expect(array1.two_sum).to eq([])
     end 
-  end  
+  end
+  
+  describe "#my_transpose" do 
+    let(:matrix) { [[0, 1, 2], [3, 4, 5], [6, 7, 8]] }
+    # first array = length 3 
+    # inside array = length 2
+    # after transposing
+    # first array = length 2
+    # inside array = length 3 
+    it "should have the correct size after transposing" do 
+      expect(matrix.my_transpose.size).to eq(matrix.transpose.size)
+    end 
+    
+    it "why rspec why" do 
+      expect(matrix.my_transpose[0].size).to eq(matrix.transpose[0].size)
+    end 
+    
+    it "should transpose a matrix" do  
+      expect(matrix.my_transpose).to eq(matrix.transpose)
+    end 
+    
+    it "should not use the built in transpose method" do 
+      expect(matrix.my_transpose).to_not receive(:transpose)
+    end 
+  end
+  
+  describe "#stock_picker" do 
+    let(:stocks) { [0,7,3,4,2,1,5,6] }
+    let(:badstocks) { [0] }
+    #array[0] = stock 1
+    #array[1] = stock 2
+    #array[2] = stock 3
+    #array[0][0] = stock 1's price on day 1 
+    # should return [0, 2], [0, 2], [0, 2]
+        
+    it "should return an array with when to buy the stock, and when to sell it" do 
+      expect(stocks.stock_picker).to eq([0,1])
+    end 
+    
+    it 'needs to buy the stock before it can sell the stock' do 
+      temp = stocks.stock_picker
+      expect(temp[1]).to be > temp[0]
+    end 
+    
+    it 'should return an error if less than 2 days are given for a stock' do 
+      
+      expect {badstocks.stock_picker}.to raise_error("Need at least two days of data for all stocks!")
+    end 
+    
+  end 
 end  
+
