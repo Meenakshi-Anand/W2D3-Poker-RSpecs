7af586c003fe932d8bd29d4fd14f409b76ac21ee
Meenakshi <abhi@codeanand.com>
2018-04-25 13:47:45 -0700

    added  two_sum

--- W2D3/TDD/lib/problems.rb
+++ W2D3/TDD/lib/problems.rb
@@ -4,6 +4,16 @@ class Array
     self.my_uniq
   end
   
+  def two_sum
+    results = [] 
+    size.times.each do |start| 
+      (start + 1...size).each do |stop|
+        results << [start, stop] if self[start] + self[stop] == 0 
+      end
+    end
+    results     
+  end 
+  
   protected
    
   def my_uniq
@@ -13,5 +23,8 @@ class Array
     end
     results  
   end
-    
+  
+  
 end 
+
+
--- W2D3/TDD/spec/problems_spec.rb
+++ W2D3/TDD/spec/problems_spec.rb
@@ -1,17 +1,31 @@
 require 'problems'
 require 'rspec'
 
-describe "#remove_dups" do
-  subject(:array) { Array.new }
+describe "Array Exercises" do
+  subject(:array) { Array.new } 
   
   before(:each) do 
     array = [1,2,1,3,3]
-  end 
-  it "should remove duplicate elements from an array" do 
-    expect(array.remove_dups).to eq(array.uniq)
-  end 
-  it "should not use built in function uniq" do 
-    expect(array).to_not receive(:uniq)
-    array.remove_dups
-  end 
+  end
+  
+  describe "#remove_dups" do
+    it "should remove duplicate elements from an array" do 
+      expect(array.remove_dups).to eq(array.uniq)
+    end
+     
+    it "should not use built in function uniq" do 
+      expect(array).to_not receive(:uniq)
+      array.remove_dups
+    end   
+  end
+    
+  describe "#two_sum" do 
+    it "should return the indices of elements that add up to 0" do 
+      array = [1, 3, -1, -2, 4, -3, 2]
+      expect(array.two_sum).to eq([[0,2], [1, 5], [3,6]])
+    end
+    it "should return an empty array if no two indices ad up to 0" do 
+      expect(array.two_sum).to eq([])
+    end 
+  end  
 end  
